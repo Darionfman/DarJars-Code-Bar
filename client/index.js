bb00e7dda3a00ef3a9f7371814f81ce0ea07f5dd
Darion Freeman <darionfman@gmail.com>
2016-12-08 14:45:46 -0600

    Adds client side code for react and react router

---
+++ client/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import App from './components/App'
+import List from './components/List'
+import AddSnippet from './components/AddSnippet'
+import SnippetPage from './components/SnippetPage'
+
+render(
+  <Provider store={ store } >
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={List}/>
+          <Route path="/add" component={AddSnippet} />
+          <Route path="/snippet/:id" component={SnippetPage} />
+        </Route>
+      </Router>
+    </Provider>,
+  document.getElementById('app')
+)
+
+module.hot.accept()
