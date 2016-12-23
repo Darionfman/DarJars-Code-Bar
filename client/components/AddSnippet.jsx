21856d4f0d2fc6fd33fd66e879fc64eb2f02ead4
Darion Freeman <darionfman@gmail.com>
2016-12-22 18:45:17 -0600

    adds some rendering and basic functions for the addsnippet component. Still working on refs

--- client/components/AddSnippet.jsx
+++ client/components/AddSnippet.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {observer} from 'mobx-react';
+import {observer} from 'mobx-react'
+import * as bs from "react-bootstrap"
 
 @observer
 export default class AddSnippet extends React.Component {
@@ -9,8 +10,38 @@ export default class AddSnippet extends React.Component {
   componentDidMount() {
     console.log('this.props')
   }
+  FieldGroup({ id, label, help, ...props }) {
+    return (
+      <bs.FormGroup controlId={id}>
+        <bs.ControlLabel>{label}</bs.ControlLabel>
+        <bs.FormControl {...props} />
+        {help && <bs.HelpBlock>{help}</bs.HelpBlock>}
+      </bs.FormGroup>
+    )
+  }
+  submitValues(e){
+    e.preventDefault()
+    console.log(`the refs are not working for some reason`)
+  }
   render(){
-    return <div>no</div>
+    let FieldGroup = this.FieldGroup
+    return (
+      <bs.Grid>
+        <form onSubmit={(e) => this.submitValues(e)}>
+          <bs.Row>
+            <bs.Col md={12}>
+            <FieldGroup
+              id='Title'
+              type='text'
+              label='Snippet Title'
+              placeholder='Insert Snippet Title'
+              defaultValue=''
+            />
+            </bs.Col>
+          </bs.Row>
+        </form>
+      </bs.Grid>
+      )
   }
 
 }
